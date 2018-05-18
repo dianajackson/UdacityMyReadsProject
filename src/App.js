22d42f3c7ff95274f95bd6d94df40a436c19084c
root <info@web3d-design.com>
2018-05-18 05:09:48 +0000

    Create Bookshelf component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Book from './Book'
+import Bookshelf from './Bookshelf'
 
 class BooksApp extends React.Component {
   state = {
@@ -45,34 +45,9 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-          <Book/>
-                      <Book/>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book/>
-                     <Book/>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <Book/>
-                     <Book/>
-                      <Book/>
-                    </ol>
-                  </div>
-                </div>
+          		<Bookshelf name="Currently Reading"/>
+          		<Bookshelf name="Want To Read"/>
+          		<Bookshelf name="Read"/>
               </div>
             </div>
             <div className="open-search">
